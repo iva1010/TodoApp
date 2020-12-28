38b615ad94ab5303ad306e22bd864c93b2b80afe
Aleksandr <lau71776@gmail.com>
2020-12-28 11:56:54 +0700

    add task list and clear template

--- src/app/service/data-handler.service.ts
+++ src/app/service/data-handler.service.ts
@@ -1,15 +1,21 @@
- import { Injectable } from '@angular/core';
- import {Category} from '../model/Category';
- import {TestData} from '../data/TestData';
+import {Injectable} from '@angular/core';
+import {Category} from '../model/Category';
+import {TestData} from '../data/TestData';
+import {Task} from '../model/Task';
 
 @Injectable({
   providedIn: 'root'
 })
 export class DataHandlerService {
 
-  constructor() { }
+  constructor() {
+  }
 
   getCategoties(): Category[] {
     return TestData.categories;
+  }
+
+  getTask(): Task[] {
+    return TestData.tasks;
   }
 }
